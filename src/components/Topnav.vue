68cd20754492aeba767b81df610ab70d5fe5b5f2
angmieee <firemao96@gmail.com>
2021-08-03 22:38:14 +0800

    路由切换关闭浮层

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleMenu">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside" @click="toggleMenu"></span>
   </div>
 </template>
 <script lang="ts">
@@ -14,12 +15,12 @@ export default {
     const menuVisible = inject<Ref<Boolean>>("menuVisible");
 
     const toggleMenu = () => {
-      menuVisible.value = !menuVisible.value
-    }
+      menuVisible.value = !menuVisible.value;
+    };
 
     return {
-      toggleMenu
-    }
+      toggleMenu,
+    };
   },
 };
 </script>
@@ -40,6 +41,27 @@ export default {
     flex-wrap: nowrap;
     > li {
       margin: 0 1em;
+    }
+  }
+  > .toggleAside {
+    display: none;
+    width: 24px;
+    height: 24px;
+    background: red;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  @media (max-width: 500px) {
+    > .menu {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggleAside {
+      display: inline-block;
     }
   }
 }
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -19,21 +19,23 @@
           </li>
         </ol>
       </aside>
-      <main>主内容</main>
+      <main>
+        <router-view></router-view>
+      </main>
     </div>
   </div>
 </template>
 <script lang="ts">
 import Topnav from "../components/Topnav.vue";
-import { inject, Ref } from 'vue';
+import { inject, Ref } from "vue";
 export default {
   components: { Topnav },
   setup() {
     const menuVisible = inject<Ref<Boolean>>("menuVisible");
-    
+
     return {
-      menuVisible
-    }
+      menuVisible,
+    };
   },
 };
 </script>
@@ -42,10 +44,7 @@ aside {
   background: lightblue;
   width: 150px;
   padding: 16px;
-  position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
+
   > h2 {
     margin-bottom: 4px;
   }
@@ -53,6 +52,12 @@ aside {
     > li {
       padding: 4px 0;
     }
+  }
+  @media (max-width: 500px) {
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
   }
 }
 </style>
