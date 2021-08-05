a2f7adbf934a6cf209f16e076117634fb0983274
angmieee <firemao96@gmail.com>
2021-08-05 22:08:15 +0800

    添加测试 Button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    buttondemo
-  </div>
+	<div>
+		<Button
+      @click="onClick"
+      @focus="onClick"
+      @mouseover="onClick">按钮</Button>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+	</div>
 </template>
+
+<script>
+	import Button from "../lib/Button.vue";
+
+	export default {
+		components: {
+			Button,
+		},
+		setup() {
+      const onClick = () => {
+        console.log('button');
+      }
+
+      return {
+        onClick
+      }
+    }
+	};
+</script>
