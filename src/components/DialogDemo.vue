649fb444f54debf37d6cd94b309c15d8ece16969
angmieee <firemao96@gmail.com>
2021-08-10 00:04:39 +0800

    Dialog 组件支持 title content 具名插槽

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -3,11 +3,19 @@
 		<h1>Dialog 示例</h1>
 		<h2>示例1</h2>
 		<Button @click="toggle">toggle</Button>
-		<Dialog 
-      :maskClosable="false" 
-      v-model:visible="visible"
-      :onOk="()=>visible = false"
-      :onCancel="()=>visible = false" />
+		<Dialog
+			:maskClosable="false"
+			v-model:visible="visible"
+			:onOk="() => (visible = false)"
+			:onCancel="() => (visible = false)"
+		>
+			<template v-slot:title>
+				<h2>标题</h2>
+			</template>
+			<template v-slot:content>
+				<p>这里是内容</p>
+			</template>
+		</Dialog>
 	</div>
 </template>
 
