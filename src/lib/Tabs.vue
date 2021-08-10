f1ad244c066ea88f529a59b519443b86522d2593
angmieee <firemao96@gmail.com>
2021-08-10 22:42:33 +0800

    Tabs 样式提交

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,25 +1,61 @@
 <template>
-	<div>
-        <div v-for="(item, index) in titles" :key="index" >{{item}}</div>
-        <component v-for="(item, index) in defaults" :is="item" :key="index"></component>
-    </div>
+	<div class="gulu-tabs">
+        <div class="gulu-tabs-nav">
+		    <div class="gulu-tabs-nav-item" v-for="(item, index) in titles" :key="index">{{ item }}</div>
+        </div>
+        <div class="gulu-tabs-content">
+            <component
+                class="gulu-tabs-content-item"
+                v-for="(item, index) in defaults"
+                :is="item"
+                :key="index"
+            ></component>
+        </div>
+	</div>
 </template>
 
 <script lang="ts">
 	import Tab from "../lib/Tab.vue";
 	export default {
-        setup(props, context) {
-            const defaults = context.slots.default()
-            defaults.forEach(tag=>{
-                if (tag.type !== Tab) {
-                    throw new Error("子标签必须为 Tab");
-                }
-            })
-            const titles = defaults.map(tag=>tag.props.title)
-            return {
-                defaults,
-                titles
-            }
-        }
-    };
+		setup(props, context) {
+			const defaults = context.slots.default();
+			defaults.forEach((tag) => {
+				if (tag.type !== Tab) {
+					throw new Error("子标签必须为 Tab");
+				}
+			});
+			const titles = defaults.map((tag) => tag.props.title);
+			return {
+				defaults,
+				titles,
+			};
+		},
+	};
 </script>
+
+<style lang="scss">
+	$blue: #40a9ff;
+	$color: #333;
+	$border-color: #d9d9d9;
+	.gulu-tabs {
+		&-nav {
+			display: flex;
+			color: $color;
+			border-bottom: 1px solid $border-color;
+			&-item {
+				padding: 8px 0;
+				margin: 0 16px;
+				cursor: pointer;
+				&:first-child {
+					margin-left: 0;
+				}
+				&.selected {
+					color: $blue;
+				}
+			}
+		}
+		&-content {
+			padding: 8px 0;
+		}
+	}
+</style>
