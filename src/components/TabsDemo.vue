33e6e55d5c0e6ddb5516139a82321d18d4da85d8
angmieee <firemao96@gmail.com>
2021-08-10 21:59:49 +0800

    context.slots.default 限制子标签类型

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,5 +1,23 @@
 <template>
+	<div>Tabs 示例</div>
+  <h2>示例1</h2>
   <div>
-    tabsdemo
+    <Tabs>
+      <Tab>aaa</Tab>
+      <Tab>aaa</Tab>
+    </Tabs>
   </div>
 </template>
+
+<script lang="ts">
+	import Tab from "../lib/Tab.vue";
+	import Tabs from "../lib/Tabs.vue";
+
+	export default {
+		components: {
+			Tab,
+			Tabs,
+		},
+		setup() {},
+	};
+</script>
---
+++ src/lib/Tabs.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+        Tabs
+    </div>
+</template>
+
+<script lang="ts">
+	import Tab from "../lib/Tab.vue";
+	export default {
+        setup(props, context) {
+            const defaults = context.slots.default()
+            defaults.forEach(tag=>{
+                if (tag.type !== Tab) {
+                    throw new Error("子标签必须为 Tab");
+                }
+            })
+            return {
+                defaults
+            }
+        }
+    };
+</script>
