80a483d27cf94bed1b43484b998516e772d8a640
angmieee <firemao96@gmail.com>
2021-08-24 16:04:40 +0800

    优化代码逻辑，抽离出 Demo 组件

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,85 +1,28 @@
 <template>
 	<div>
 		<h1>Switch 组件示例</h1>
-		<div class="demo">
-			<h2>常规用法</h2>
-			<div class="demo-component">
-				<Component :is="SwitchExample1" />
-			</div>
-			<div class="demo-actions">
-				<Button>查看代码</Button>
-			</div>
-			<div class="demo-code">
-				<pre class="language-html" v-html="Prism.highlight(SwitchExample1.__sourceCode, Prism.languages.html, 'html')" />
-			</div>
-		</div>
-		<div class="demo">
-			<h2>支持 disabled</h2>
-			<div class="demo-component">
-				<Component :is="SwitchExample2" />
-			</div>
-			<div class="demo-actions">
-				<Button>查看代码</Button>
-			</div>
-			<div class="demo-code">
-				<pre class="language-html" v-html="Prism.highlight(SwitchExample1.__sourceCode, Prism.languages.html, 'html')" />
-			</div>
-		</div>
+		<Demo :component="SwitchExample1" />
+		<Demo :component="SwitchExample2" />
 	</div>
 </template>
 
 <script lang="ts">
-	import { ref } from "vue";
-	import Switch from "../lib/Switch.vue";
+	import Demo from "../components/Demo.vue";
 	import SwitchExample1 from "./SwitchExample1.vue";
 	import SwitchExample2 from "./SwitchExample2.vue";
-	import 'prismjs';
-	import 'prismjs/themes/prism.css';
-	const Prism = (window as any).Prism;
 
 	export default {
 		components: {
-			Switch,
+			Demo
 		},
 		setup() {
-			const checked = ref(false);
-			console.log(SwitchExample1);
-			
 			return {
-				checked,
 				SwitchExample1,
 				SwitchExample2,
-				Prism
 			};
 		},
 	};
 </script>
 
 <style lang="scss" scoped>
-	$border-color: #d9d9d9;
-	.demo {
-		border: 1px solid $border-color;
-		margin: 16px 0 32px;
-		> h2 {
-			font-size: 20px;
-			padding: 8px 16px;
-			border-bottom: 1px solid $border-color;
-		}
-		&-component {
-			padding: 16px;
-		}
-		&-actions {
-			padding: 8px 16px;
-			border-top: 1px dashed $border-color;
-		}
-		&-code {
-			padding: 8px 16px;
-			border-top: 1px dashed $border-color;
-			> pre {
-				line-height: 1.1;
-				font-family: Consolas, "Courier New", Courier, monospace;
-				margin: 0;
-			}
-		}
-	}
 </style>
