ad64a6f2c83c9927c9e6f8e5e044e57c9cb975e0
angmieee <firemao96@gmail.com>
2021-09-02 14:39:20 +0800

    Demo & Doc 样式变更

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -2,27 +2,27 @@
 	<div
 		class="demo"
 		:class="{ hover: isHover }"
-		@mouseover="isHover = true"
-		@mouseout="isHover = false"
+		@mouseenter="isHover = true"
+		@mouseleave="isHover = false"
 	>
 		<h2>{{ component.__sourceCodeTitle }}</h2>
 		<div class="demo-component">
 			<Component :is="component" />
 		</div>
-		<div class="demo-actions" @click="toggleVisible">
-			<svg v-show="!codeVisible">
-				<use xlink:href="#icon-down"></use>
-			</svg>
-			<svg v-show="codeVisible">
-				<use xlink:href="#icon-up"></use>
-			</svg>
-			<span v-show="isHover">
-				<span v-show="!codeVisible">显示代码</span>
-				<span v-show="codeVisible">隐藏代码</span>
-			</span>
+		<div class="demo-code" :style="{ height: height + 'px' }">
+			<pre ref="codeRef" class="language-html" v-html="html" />
 		</div>
-		<div class="demo-code" v-if="codeVisible">
-			<pre class="language-html" v-html="html" />
+		<div class="demo-actions" @click="toggleVisible">
+			<svg>
+				<use
+					:xlink:href="!codeVisible ? '#icon-down' : '#icon-up'"
+				></use>
+			</svg>
+			<transition name="fade">
+				<span v-show="isHover">{{
+					!codeVisible ? "显示代码" : "隐藏代码"
+				}}</span>
+			</transition>
 		</div>
 	</div>
 </template>
@@ -31,7 +31,7 @@
 	import Button from "../lib/Button.vue";
 	import "prismjs";
 	import "prismjs/themes/prism.css";
-	import { computed, ref } from "vue";
+	import { computed, ref, onMounted } from "vue";
 	const Prism = (window as any).Prism;
 
 	export default {
@@ -47,9 +47,17 @@
 		setup(props) {
 			const codeVisible = ref(false);
 
+			const codeRef = ref(null);
+
+			const computedHeight = ref(0);
+
 			const toggleVisible = () => {
 				codeVisible.value = !codeVisible.value;
 			};
+
+			onMounted(() => {
+				computedHeight.value = codeRef.value.getBoundingClientRect().height;
+			});
 
 			const isHover = ref(false);
 
@@ -61,9 +69,15 @@
 				);
 			});
 
+			const height = computed(() => {
+				return codeVisible.value ? computedHeight.value : 0;
+			});
+
 			return {
 				Prism,
 				html,
+				height,
+				codeRef,
 				codeVisible,
 				toggleVisible,
 				isHover,
@@ -88,7 +102,7 @@
 		}
 		&-actions {
 			padding: 8px 16px;
-			border-top: 1px dashed $border-color;
+			border-top: 1px solid $border-color;
 			cursor: pointer;
 			text-align: center;
 			color: #d3dce6;
@@ -99,8 +113,25 @@
 				fill: #d3dce6;
 			}
 			> span {
+				position: absolute;
 				transition: all 0.3s;
-				display: inline-block;
+				transform: translateX(-30px);
+				&.fade-enter {
+					opacity: 0;
+					transform: translateX(10px);
+				}
+				&.fade-enter-to {
+					opacity: 1;
+					transform: translateX(-30px);
+				}
+				&.fade-leave {
+					opacity: 1;
+					transform: translateX(-30px);
+				}
+				&.fade-leave-to {
+					opacity: 0;
+					transform: translateX(0px);
+				}
 			}
 			&:hover {
 				background-color: #f9fafc;
@@ -113,8 +144,12 @@
 			}
 		}
 		&-code {
-			padding: 8px 16px;
-			border-top: 1px dashed $border-color;
+			transition: height 0.2s;
+			overflow: hidden;
+			.language-html {
+				background-color: #fafafa;
+			}
+
 			> pre {
 				line-height: 1.1;
 				font-family: Consolas, "Courier New", Courier, monospace;
@@ -122,12 +157,12 @@
 			}
 		}
 		&.hover {
-			transition: 0.2s;
+			transition: all 0.3s;
 			box-shadow: 0 0 8px 0 rgb(232 237 250 / 60%),
 				0 2px 4px 0 rgb(232 237 250 / 50%);
 			.demo-actions {
 				> svg {
-					transform: translateX(-10px);
+					transform: translateX(-35px);
 				}
 			}
 		}
--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,7 @@
 	<div class="layout">
 		<Topnav :toggleMenuVisible="true" class="nav" />
 		<div class="content">
-			<aside v-if="menuVisible">
+			<aside :class="{open: menuVisible}">
 				<h2>文档</h2>
 				<ol>
 					<li>
@@ -20,16 +20,28 @@
 				<h2>组件列表</h2>
 				<ol>
 					<li>
-						<router-link to="/doc/switch">Switch 组件</router-link>
+						<router-link to="/doc/switch">
+							<span>Switch</span>
+							<span class="chinese">开关</span>
+						</router-link>
 					</li>
 					<li>
-						<router-link to="/doc/button">Button 组件</router-link>
+						<router-link to="/doc/button">
+							<span>Button</span>
+							<span class="chinese">按钮</span>
+						</router-link>
 					</li>
 					<li>
-						<router-link to="/doc/dialog">Dialog 组件</router-link>
+						<router-link to="/doc/dialog">
+							<span>Dialog</span>
+							<span class="chinese">对话框</span>
+						</router-link>
 					</li>
 					<li>
-						<router-link to="/doc/tabs">Tabs 组件</router-link>
+						<router-link to="/doc/tabs">
+							<span>Tabs</span>
+							<span class="chinese">标签页</span>
+						</router-link>
 					</li>
 				</ol>
 			</aside>
@@ -54,6 +66,7 @@
 	};
 </script>
 <style lang="scss" scoped>
+	$fontColor: #52b69b;
 	.layout {
 		display: flex;
 		flex-direction: column;
@@ -64,7 +77,7 @@
 		> .content {
 			flex-grow: 1;
 			padding-top: 60px;
-			padding-left: 156px;
+			padding-left: 280px;
 			@media (max-width: 500px) {
 				padding-left: 0;
 			}
@@ -82,36 +95,70 @@
 		}
 	}
 	aside {
+		background-color: #fff;
+		// box-shadow: 3px 0 3px rgba(#333, 0.1);
+		box-shadow: 0 2px 8px #f0f1f2;
 		border-right: 1px solid #f0f0f0;
-		width: 150px;
+		width: 280px;
 		padding: 16px 0;
 		position: fixed;
 		top: 0;
 		left: 0;
 		padding-top: 70px;
 		height: 100%;
+		overflow-x: hidden;
 		z-index: 1;
+		transition: all 0.4s cubic-bezier(0.4, 0, 0, 1);
+		@media (max-width: 500px) {
+			transform: translateX(-300px);
+			box-shadow: 0 0 10px rgba(0,0,0,0.2);
+			&.open {
+				transform: translateX(0px);
+			}
+		}
 		> h2 {
 			margin-bottom: 4px;
-			padding: 0 16px;
+			padding: 0 50px;
 		}
 		> ol {
 			> li {
 				> a {
 					display: block;
-					padding: 4px 16px;
+					position: relative;
+					padding: 8px 50px;
 					text-decoration: none;
+					transition: color 0.3s;
+					font-size: 16px;
+					&:hover {
+						color: $fontColor;
+					}
+					> span {
+						&.chinese {
+							margin-left: 6px;
+							font-weight: 400;
+							font-size: 12px;
+							opacity: 0.67;
+						}
+					}
 				}
 				.router-link-active {
 					// background: #d2f8eb;
 					background-color: #f0fcef;
-					color: #52b69b;
-					&::after {
-						position: absolute;
-						top: 0;
-						right: 0;
-						bottom: 0;
-						border-right: 3px solid #1890ff;
+					border-right: 3px solid $fontColor;
+					color: $fontColor;
+					@media (min-width: 500px) {
+						border-right: none;
+						&::after {
+							content: "";
+							display: block;
+							animation: bdrolate 0.8s;
+							position: absolute;
+							top: 0;
+							right: 0;
+							width: 3px;
+							height: 40px;
+							background-color: $fontColor;
+						}
 					}
 				}
 			}
@@ -119,5 +166,14 @@
 	}
 	main {
 		overflow: auto;
+	}
+
+	@keyframes bdrolate {
+		0% {
+			transform: rotateX(90deg);
+		}
+		100% {
+			transform: rotateX(0deg);
+		}
 	}
 </style>
