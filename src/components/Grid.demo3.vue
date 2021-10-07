e946afd9c5b54c47abb5a45b65921295cadce695
angmieee <firemao96@gmail.com>
2021-10-07 20:48:43 +0800

    Grid 增加区块间隔

---
+++ src/components/Grid.demo3.vue
@@ -0,0 +1,45 @@
+<demo>
+区块间隔
+</demo>
+<template>
+	<Row gutter="16">
+		<Col class="gutter-row" span="6">
+            <div :style="style">col-6</div>
+        </Col>
+		<Col class="gutter-row" span="6">
+            <div :style="style">col-6</div>
+        </Col>
+		<Col class="gutter-row" span="6">
+            <div :style="style">col-6</div>
+        </Col>
+		<Col class="gutter-row" span="6">
+            <div :style="style">col-6</div>
+        </Col>
+	</Row>
+</template>
+
+<script>
+    import { ref } from "vue";
+	import { Row, Col } from "../lib/index";
+	export default {
+		components: {
+			Row,
+			Col,
+		},
+		setup() {
+			const style = ref({ background: "#0092ff", padding: "8px 0" });
+            return {
+                style
+            }
+		},
+	};
+</script>
+
+<style lang='scss' scoped>
+    .gutter-row {
+        background: #fff;
+        &:nth-child(2n+1) {
+            background: #fff;
+        }
+    }
+</style>
--- src/components/GridDemo.vue
+++ src/components/GridDemo.vue
@@ -2,12 +2,14 @@
 	<h1>Grid 示例</h1>
 	<Demo :component="GridDemo1" />
 	<Demo :component="GridDemo2" />
+	<Demo :component="GridDemo3" />
 </template>
 
 <script lang="ts">
 	import Demo from "./Demo.vue";
 	import GridDemo1 from "./Grid.demo1.vue";
 	import GridDemo2 from "./Grid.demo2.vue";
+	import GridDemo3 from "./Grid.demo3.vue";
 
 	export default {
 		components: {
@@ -16,7 +18,8 @@
 		setup() {
 			return {
 				GridDemo1,
-				GridDemo2
+				GridDemo2,
+				GridDemo3,
 			};
 		},
 	};
